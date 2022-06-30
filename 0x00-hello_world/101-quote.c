594d1ed8de1d0a5cdd6164bb9ddc8ce7ae7b4f79
Ghumdia <bosass80@gmail.com>
2022-06-30 15:19:13 -0700

    h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,16 @@
 #include <string.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
+ *
+ * Description: 
+ *
  * Return: 1 (Success)
  */
-
 int main(void)
 {
-	char *text="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	int length=strlen(text);
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int length = strlen(text);
 
 	write(2, text, length);
 	return (1);	
