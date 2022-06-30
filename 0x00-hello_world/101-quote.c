8638faea9af8cb41250c306b2bad7dd9b6835871
Ghumdia <bosass80@gmail.com>
2022-06-30 15:26:44 -0700

    h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,8 +12,8 @@
 int main(void)
 {
 	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	int length = strlen(text);
+	int le = strlen(text);
 
-	write(2, text, length);
-	return (0);
+	write(2, text, le);
+	return (1);
 }
