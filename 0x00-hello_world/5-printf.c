d48cf4eeccfae12ad24b4205a6ec4c553aef92fe
Ghumdia <bosass80@gmail.com>
2022-06-30 15:34:53 -0700

    m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of art,"
+ * Description:
  *
  * Return: Always 0 (Success)
  */
@@ -11,6 +11,4 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
-
 }
