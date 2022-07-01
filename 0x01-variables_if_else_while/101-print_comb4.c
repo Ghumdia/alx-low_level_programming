823fba6ae404f7c9f9666d5006008b0797165f43
Ghumdia <bosass80@gmail.com>
2022-07-01 11:12:56 -0700

    Commited

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing three digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i,e,q;
+
+	i = 48;
+	e = 48;
+	q = 48;
+
+	while (i < 58)
+	{
+		e = i + 1;
+		while (e < 58)
+		{
+			q = e + 1;
+			while (q < 58)
+			{
+				putchar(i);
+				putchar(e);
+				putchar(q);
+				if (1 < 55 || e < 56 || q < 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				q++;
+			}
+			e++;
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
