f2c39ea0135f6a1e9461d4fa0085bcfe01abb8cc
Ghumdia <bosass80@gmail.com>
2022-07-01 08:20:50 -0700

    Commited

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
  /**
  * main - determine if number is positive or negative
  *
- * Description: prints "whether the number is 0, - or +"
+ * Description: prints whether the number is 0, - or +
+ * it is executed
  *
  * Return 0 (Success)
  */
