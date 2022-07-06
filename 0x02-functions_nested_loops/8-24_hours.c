81aa79d64a195a0d934c983edfb42fd1682d39ab
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2022-07-06 14:15:32 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * 24_hours - prints every minute
+ * jack_bauer - prints every minute of the day
  *
- * Description: prints every minute of song
+ * Description: prints every minute of the day
  *
- * Return: noting
+ * Return: void
  */
 
 void jack_bauer(void)
@@ -13,18 +13,18 @@ void jack_bauer(void)
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
 	hours_max = 58;
-	hours_tens = 0;
+	hours_tens = '0';
 	while (hours_tens < '3')
 	{
 		if (hours_tens == '2')
 		{
-			hours_max == '4';
+			hours_max = '4';
 		}
 		hours_ones = '0';
 		while (hours_ones < hours_max)
 		{
 			minutes_tens = '0';
-			while (minutes_tens <'6')
+			while (minutes_tens < '6')
 			{
 				minutes_ones = '0';
 				while (minutes_ones < 58)
