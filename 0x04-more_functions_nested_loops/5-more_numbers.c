352ab31bf83514676fa5a6aeb0d2fdeb208e0055
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2022-07-07 15:12:27 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,37 @@
 #include "main.h"
 
 /**
- * more_numbers - prints most numbers
+ * more_numbers - prints 0-14 ten times
  *
  * Return: void
  */
 
 void more_numbers(void)
 {
-	int i, c;
+	int line, n, repeat, limit;
 
-	for (i = 0; i < 10; i++)
+	n = '0';
+	repeat = 0;
+	limit = '9';
+
+	for (line = 0; line < 10; line++)
 	{
-		c = '0';
-		for (c <= '14'; c++)
+		while (repeat < 2)
 		{
-			_putchar(c);
+			while (n <= limit)
+			{
+				if (limit == '4')
+					_putchar('1');
+				_putchar(n);
+				n++;
+			}
+			repeat++;
+			limit = '4';
+			n = '0';
 		}
+		_putchar('\n');
+		repeat = 0;
+		n = '0';
+		limit = '9';
 	}
-	_putchar('\n');
 }
