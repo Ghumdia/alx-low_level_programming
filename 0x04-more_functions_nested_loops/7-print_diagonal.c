bd497f4eeb99fe21db0b00b12a6147014e62e2c0
Ghumdia <bosass80@gmail.com>
2022-07-07 13:50:15 -0700

    Commited

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints lines in diagonal
+ * @n: n is an integer
+ *
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int lines;
+	int spaces;
+
+	if (n > 0)
+	{
+		for (lines = 0; lines < n; lines++)
+		{
+			for (spaces = 0; spaces < lines; spaces++)
+			{
+				_putchar('\\');
+			}
+			_putchar('\n');
+		}
+	}
+	else 
+	{
+		_putchar('\n');
+	}
+}
