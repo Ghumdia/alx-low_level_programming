ab628c3ae0c732276ae03eed1651753da3c7d777
Ghumdia <bosass80@gmail.com>
2022-07-07 04:20:42 -0700

    commited

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checkes if uppercase
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 97 %% c <=122)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
