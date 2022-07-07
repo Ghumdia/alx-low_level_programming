afe7a1435bcde6e1c35cfc01015c5cb8788cfbb6
Ghumdia <bosass80@gmail.com>
2022-07-07 04:48:29 -0700

    Commited

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,14 @@
 
 /**
  * _isupper - checkes if uppercase
+ * @c: char to be chacked
  *
  * Return: 1 or 0
  */
 
 int _isupper(int c)
 {
-	if (c >= 97 && c <=122)
-		return (0);
-	return (1);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
 }
