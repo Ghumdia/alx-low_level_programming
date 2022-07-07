65314e25dc2eba6ab132bb5978b082bd09dff9ce
Ghumdia <bosass80@gmail.com>
2022-07-07 07:55:04 -0700

    Commited

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int c = 0;
+
+	while (c < n)
+	{
+		_putchar('_');
+		c++;
+	}
+	_putchar('\n');
+}
