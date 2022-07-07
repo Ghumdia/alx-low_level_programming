db22da242bf58c1048c58a2458d74ed6248f4c9c
Ghumdia <bosass80@gmail.com>
2022-07-07 06:06:40 -0700

    Commited

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int c = 48;
+
+	while (c < 58)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar("\n");
+}
