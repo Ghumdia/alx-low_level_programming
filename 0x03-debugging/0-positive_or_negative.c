9bd96376820a3a0636a7b1b1205a3cf3dac97301
Ghumdia <bosass80@gmail.com>
2022-07-08 12:19:57 -0700

    Commited

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -1,14 +1,28 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Description: prints zero
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
 int n;
 n = 0;
 if (n < 0)
 {
-printf("%i is negative\n", n);                             }
+printf("%i is negative\n", n);
+}
 else if (n > 0)
 {
 printf("%i is positive\n", n);
 }
 else
-{                                                          printf("%i is zero\n", n);                                 }
+{
+printf("%i is zero\n", n);
+}
 return (0);
+}
