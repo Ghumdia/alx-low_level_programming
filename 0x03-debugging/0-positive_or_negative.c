567c221ebf22b78fae8f99088b1938657559b7f1
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2022-07-08 19:18:04 +0100

    Update 0-positive_or_negative.c

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -1,2 +1,14 @@
-#:wq
+#include "main.h"
 
+int n;
+n = 0;
+if (n < 0)
+{
+printf("%i is negative\n", n);                             }
+else if (n > 0)
+{
+printf("%i is positive\n", n);
+}
+else
+{                                                          printf("%i is zero\n", n);                                 }
+return (0);
