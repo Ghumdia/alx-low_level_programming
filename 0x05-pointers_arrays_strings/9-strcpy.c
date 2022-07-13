c892aead0684caf1d97e61d5a0b7ed4de674d740
Ghumdia <bosass80@gmail.com>
2022-07-13 13:52:14 -0700

    Commited

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - nbr nvjif kg
+ * @dest: jiif
+ * @scr: nif
+ *
+ * Return: nfunv
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
