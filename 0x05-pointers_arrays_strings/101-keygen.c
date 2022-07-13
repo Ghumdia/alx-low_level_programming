8f5a58a5101618d5e19732064c54a80e83c7b607
Ghumdia <bosass80@gmail.com>
2022-07-13 15:01:28 -0700

    PASS

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -15,13 +15,13 @@ int main(void)
 	int N = 10;
 	int i = 0;
 	int randm = 0;
-	srand((unsigned int)(time(NULL)));
 	char numb[1000] = "1234567890";
 	char letter[1000] = "abcdefghijklmnopqrst";
 	char Letter[1000] = "ABCDEFGHIJKLMNOP";
 	char sym[1000] = "$%&@!&";
 	char password[10];
 	randm = rand() % 4;
+	sran((unsigned int)(time(NULL)));
 
 	for (i = 0; i < N; i++)
 	{
