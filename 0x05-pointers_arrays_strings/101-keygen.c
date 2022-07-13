676d2fd00688146e445a6b084d71618706f47c6d
Ghumdia <bosass80@gmail.com>
2022-07-13 15:17:18 -0700

    PASS

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,15 +12,15 @@
  */
 int main(void)
 {
-	int N = 10;
+	int N = 30;
 	int i = 0;
 	int randm = 0;
 	char numb[1000] = "1234567890";
 	char letter[1000] = "abcdefghijklmnopqrst";
-	char Letter[1000] = "ABCDEFGHIJKLMNOP";
-	/**char sym[1000] = "$%&@!&";*/
-	char password[10];
-	randm = rand() % 3;
+	char Letter[1000] = "AB CDEFGHIJKLMNOP";
+	char sym[1000] = "$%&@!&";
+	char password[30];
+	randm = rand() % 4;
 	srand((unsigned int)(time(NULL)));
 
 	for (i = 0; i < N; i++)
@@ -28,28 +28,27 @@ int main(void)
 		if (randm == 1)
 		{
 			password[i] = numb[rand() % 10];
-			randm = rand() % 3;
+			randm = rand() % 4;
 			printf("%c", password[i]);
 		}
 		else if (randm == 2)
 		{
 			password[i] = letter[rand() % 20];
-			randm = rand() % 3;
+			randm = rand() % 4;
 			printf("%c", password[i]);
 		}
 		else if (randm == 3)
 		{
 			password[i] = Letter[rand() % 16];
-			randm = rand() % 3;
+			randm = rand() % 4;
 			printf("%c", password[i]);
 		}
-		/**else
+		else
 		{
 			password[i] = sym[rand() % 6];
-			randm = rand() % 3;
+			randm = rand() % 4;
 			printf("%c", password[i]);
 		}
-		*/
 	}
 	putchar('\n');
 	return (0);
