89646c1f8b52ba5c94e07734a9b96e39b68f9fc3
Ghumdia <bosass80@gmail.com>
2022-07-13 07:44:11 -0700

    Commited

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - inserts half of the string
+ * @str: the string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, j, k;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	k = i - 1;
+
+	if (i % 2 == 0)
+	{
+		for (j = i / 2; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(str[k]);
+	}
+}
