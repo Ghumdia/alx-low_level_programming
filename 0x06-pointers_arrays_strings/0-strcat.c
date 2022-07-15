b1ba0751254d492b869e01c707b972a96456fa2b
Ghumdia <bosass80@gmail.com>
2022-07-15 06:21:09 -0700

    Commited

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 	int n = 0;
 
-	while (dest[n] != '\0')
+	while (*(dest + i))
+		i++;
+	while ((*(dest + i) = *(src + n)))
 	{
+		i++;
 		n++;
 	}
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[n + i] = src[i];
-	dest[n + i] = '\0';
-
-	return (0);
+	return (dest);
 }
