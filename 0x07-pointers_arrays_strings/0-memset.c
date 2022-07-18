850fc99a4947306c3d2a7ee066e1e2aa774ab47b
Ghumdia <bosass80@gmail.com>
2022-07-18 12:07:37 -0700

    Commited

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _memset - files up memory
+ * @s: string
+ * @n: integer
+ * @b: bytes
+ *
+ * Return: void
+ */
+
+char *_memset(char  *s, char b, unsigned int n)
+{
+	memset(s, n, b);
+	return (0);
+}
