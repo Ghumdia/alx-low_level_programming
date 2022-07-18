0ad8f0486def858e15d90d3daf45e6c321f4babc
Ghumdia <bosass80@gmail.com>
2022-07-18 12:29:35 -0700

    Commited

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies to location
+ * @dest: destination
+ * @src: source
+ * @n: ineger
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
