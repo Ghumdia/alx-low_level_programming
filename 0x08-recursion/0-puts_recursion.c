a4f1c75e13caddde31a025cfe7b3fd5d10092634
Ghumdia <bosass80@gmail.com>
2022-07-21 10:25:23 -0700

    Commited

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,10 +10,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
+		_putchar(*s);
+		_puts_recursion((s + 1));
+	}
+	else
+	{
+		_putchar('\n');
 		return;
 	}
-	printf("%c", *s);
-	_puts_recursion(s + 1);
 }
