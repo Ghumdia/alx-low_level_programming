9797276c2c739e634b4ae2cadd945a97ddf1af22
Ghumdia <bosass80@gmail.com>
2022-07-21 11:29:43 -0700

    Commited

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - square
+ * @n: integer
+ *
+ * Return: 0
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+int square(int n, int val)
+{
+	if (val * val == n)
+	{
+		return (val);
+	}
+	else if (val * val < n)
+	{
+		return (square(n, val + 1));
+	}
+	else
+	{
+		return(-1);
+	}
+}
