381ae54399f9439ddb473bc34d8e61bc0553f80d
Ghumdia <bosass80@gmail.com>
2022-07-22 12:36:02 -0700

    Commited

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: number of arguments
+ * @argv: string of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
