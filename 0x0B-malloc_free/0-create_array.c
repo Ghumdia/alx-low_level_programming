762633e747800ea123c2a2980640b4bc44c8d547
Ghumdia <bosass80@gmail.com>
2022-07-26 12:18:06 -0700

    Commited

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_array - creates an array
+ * @size: int
+ * @c: casket
+ *
+ * Return: NULL, 0 or ar
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int n;
+	char *ar;
+
+	ar = (char *)malloc(size * sizeof(char));
+	if (size == 0)
+		return (NULL);
+	else if (size != 0)
+	{
+		for (n = 0; n < size; n++)
+			*(ar + n) = c;
+		return (ar);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
