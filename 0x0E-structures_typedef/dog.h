1fef51b9d3e26a462f4995b95e7c9fda3122b72b
Ghumdia <bosass80@gmail.com>
2022-08-02 12:16:07 -0700

    Commited

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - this are the dogs details
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: various details of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
