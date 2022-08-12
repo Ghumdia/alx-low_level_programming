38400c3c4093098315c81bee97799a2d4e7122bb
Ghumdia <bosass80@gmail.com>
2022-08-12 11:37:02 -0700

    Committed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * mystartup - starts the program
+ */
+
+void __attribute__ ((constructor)) mystartup(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
