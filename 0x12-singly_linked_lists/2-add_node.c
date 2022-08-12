ba3e94134f1c3f6220a7523e09fa470c42afece4
Ghumdia <bosass80@gmail.com>
2022-08-12 09:59:08 -0700

    Committed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,6 +11,8 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+		return (NULL);
 
 	newnode->str = strdup(str);
 	newnode->len = _strlen(str);
