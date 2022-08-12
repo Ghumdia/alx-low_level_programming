2cbfaa8f0519e3b865cc1d929f5ad7d16f7bb72c
Ghumdia <bosass80@gmail.com>
2022-08-12 09:50:29 -0700

    Committed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,11 +1,36 @@
 #include "lists.h"
 
+/**
+ * add_node - function adds a new node
+ * @head: first pointer
+ * @str: string
+ *
+ * Return: address of first node
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newnode = malloc(sizeof(list_t));
 
-	newnode->str = strdup (str);
+	newnode->str = strdup(str);
+	newnode->len = _strlen(str);
 	newnode->next = *head;
 	*head = newnode;
 	return (*head);
+}
+
+/**
+ * _strlen - gets length of string
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
 }
