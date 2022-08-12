04620adf5e8d106423033075812890751960088a
Ghumdia <bosass80@gmail.com>
2022-08-12 09:31:59 -0700

    Committed

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode = malloc(sizeof(list_t));
+
+	newnode->str = strdup (str);
+	newnode->next = *head;
+	*head = newnode;
+	return (*head);
+}
